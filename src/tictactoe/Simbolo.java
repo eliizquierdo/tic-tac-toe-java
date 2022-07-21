315df92f02f811b168e900e3d4256e4e5407ce6a
eliizquierdo <eizquierdo@docente.ceibal.edu.uy>
2022-07-21 18:07:11 -0300

    Subiendo desde eclipse

--- src/tictactoe/Simbolo.java
+++ src/tictactoe/Simbolo.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//proyecto descargado desde github a traves de de eclipse git
 package tictactoe;
 
 public enum Simbolo {
